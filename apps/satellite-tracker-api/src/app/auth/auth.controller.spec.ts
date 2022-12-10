da3c0d49a8b6fa95f46812a200fd9e97ecc7db2c
2182556 <je.boellaard@student.avans.nl>
2022-12-11 00:55:42 +0100

    small changes

--- apps/satellite-tracker-api/src/app/auth/auth.controller.spec.ts
+++ apps/satellite-tracker-api/src/app/auth/auth.controller.spec.ts
@@ -98,4 +98,83 @@ describe('AuthController', () => {
             // expect(result).toEqual(new HttpException('Could not create user', HttpStatus.BAD_REQUEST));
         });
     });
+
+    describe('login', () => {
+        let validCredentials: any, invalidCredentials: any, generateToken: any;
+        let example_id: string;
+
+        beforeEach(() => {
+            validCredentials = {
+                username: 'valid',
+                password: 'validpassword',
+            };
+
+            invalidCredentials = {
+                username: 'invalid',
+                password: 'invalidpassword',
+            };
+
+            example_id = 'id12345';
+            generateToken = jest.spyOn(authService, 'generateToken').mockImplementation(async (username, password) => {
+                if (username === validCredentials.username && password === validCredentials.password) {
+                    return {
+                        token: 'token12345',
+                    };
+                }
+                return new HttpException('Invalid credentials', HttpStatus.BAD_REQUEST);
+            });
+        });
+
+        it('should call generateToken', async () => {
+            await controller.login('', validCredentials);
+            expect(generateToken).toBeCalledWith(validCredentials.username, validCredentials.password);
+        });
+
+        it('should return a token', async () => {
+            const result = await controller.login('', validCredentials);
+            expect(result).toEqual({ token: 'token12345' });
+        });
+
+        it('should return an exception if generateToken returns exception', async () => {
+            const result = await controller.login('', invalidCredentials);
+            expect(generateToken).toReturnWith(new HttpException('Invalid credentials', HttpStatus.BAD_REQUEST));
+        });
+    });
+
+    describe('getSelf', () => {
+        let identity: any, getIdentity: any;
+
+        beforeEach(() => {
+            identity = {
+                _id: 'id12345',
+                username: 'test',
+                password: 'testp',
+                emailAddress: 'test@test',
+            };
+
+            getIdentity = jest.spyOn(authService, 'getIdentity').mockImplementation(async (id) => {
+                if (id === identity._id) {
+                    return identity;
+                }
+                return new HttpException('Could not find user', HttpStatus.BAD_REQUEST);
+            });
+        });
+
+        it('should call getIdentity', async () => {
+            await controller.getSelf('', identity._id);
+            expect(getIdentity).toBeCalledWith(identity._id);
+        });
+
+        it('should return the identity', async () => {
+            const result = await controller.getSelf('', identity._id);
+            expect(result).toEqual(identity);
+        });
+
+        it('should return an exception if getIdentity returns exception', async () => {
+            const result = await controller.getSelf('', 'invalid');
+            expect(getIdentity).toReturnWith(new HttpException('Could not find user', HttpStatus.BAD_REQUEST));
+        });
+
+        // test if method is unreachable for non-logged in users
+    });
 });
