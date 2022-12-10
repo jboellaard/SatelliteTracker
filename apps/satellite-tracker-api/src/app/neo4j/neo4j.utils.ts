4c52567451d821777971e6a945eabbcde55f997b
2182556 <je.boellaard@student.avans.nl>
2022-12-10 21:27:09 +0100

    added logging statement

--- apps/satellite-tracker-api/src/app/neo4j/neo4j.utils.ts
+++ apps/satellite-tracker-api/src/app/neo4j/neo4j.utils.ts
@@ -2,6 +2,7 @@ import neo4j, { Driver } from 'neo4j-driver';
 import { Neo4jConfig } from './neo4j.config.interface';
 
 export const createDriver = async (config: Neo4jConfig) => {
+    console.log(config);
     const driver: Driver = neo4j.driver(
         `${config.scheme}://${config.host}`,
         neo4j.auth.basic(config.username, config.password)
